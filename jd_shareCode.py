df2f6f8fab935a393b69b34bc68ebdc91d002189
AnthonyMSen <1072519956@qq.com>
2020-12-09 12:34:09 +0800

    Create jd_shareCode.py

---
+++ jd_shareCode.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import json
+
+JD_COOKIE = '- JD_COOKIE='
+PLANT_BEAN_SHARECODES = '- PLANT_BEAN_SHARECODES='
+FRUITSHARECODES = '- FRUITSHARECODES='
+PETSHARECODES = '- PETSHARECODES='
+DDFACTORY_SHARECODES = '- DDFACTORY_SHARECODES='
+DREAM_FACTORY_SHARE_CODES = '- DREAM_FACTORY_SHARE_CODES='
+
+
+with open("jd_shareCode.json", 'r') as load_f:
+    load_dict = json.load(load_f)
+
+own = load_dict['own']
+frd = load_dict['frd']
+
+# get JD_COOKIE
+for i in range(len(own)):
+    if i >= 1:
+        JD_COOKIE = JD_COOKIE + '&'
+    pt_key = 'pt_key=' + own[i]['pt_key'] + ';'
+    pt_pin = 'pt_pin=' + own[i]['pt_pin'] + ';'
+    JD_COOKIE = JD_COOKIE + pt_key + pt_pin
+
+
+def get_finalcode(parameter):
+    # get full code
+    all_code = ''
+    final_code = ''
+    for j in range(len(own)):
+        if j >= 1:
+            all_code = all_code + '@'
+        all_code = all_code + own[j][parameter]
+
+    for j in range(len(frd)):
+        all_code = all_code + '@' + frd[j][parameter]
+    # combine all code
+    for j in range(len(own)):
+        if j >= 1:
+            final_code = final_code + '&'
+        final_code = final_code + all_code.replace(own[j][parameter], '')
+    final_code = final_code.replace('@@', '@')
+    final_code = final_code.lstrip('@')
+    return final_code
+
+
+PLANT_BEAN_SHARECODES = PLANT_BEAN_SHARECODES + get_finalcode('bean')
+FRUITSHARECODES = FRUITSHARECODES + get_finalcode('fruit')
+PETSHARECODES = PETSHARECODES + get_finalcode('pet')
+DDFACTORY_SHARECODES = DDFACTORY_SHARECODES + get_finalcode('ddfactory')
+DREAM_FACTORY_SHARE_CODES = DREAM_FACTORY_SHARE_CODES + get_finalcode('drfactory')
+
+# finish and print
+print(JD_COOKIE)
+print(PLANT_BEAN_SHARECODES)
+print(FRUITSHARECODES)
+print(PETSHARECODES)
+print(DDFACTORY_SHARECODES)
+print(DREAM_FACTORY_SHARE_CODES)
